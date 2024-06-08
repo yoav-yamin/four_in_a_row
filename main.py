9c75375c1479a06b2c4ca054f0bbea0634c492fc
Yoav Yamin <142584165+yoav-yamin@users.noreply.github.com>
2024-06-09 02:09:58 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from Server_socket import Server_socket
-
-def startGame():
-    server_socket = Server_socket()
-    server_socket.create_server_socket()
-
-
-if __name__ == '__main__':
-    startGame()
